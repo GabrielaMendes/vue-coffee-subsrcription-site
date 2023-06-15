d4b34d971fcbe57719e20f24b6ca2f3fcc26e7ff
Gabriela <gdemelo.mendes@gmail.com>
2023-06-15 11:19:59 -0300

    style: added custom font and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        barlow: ["'Barlow', sans-serif"],
+        fraunces: ["'Fraunces', serif"],
+      },
+      colors: {
+        "light-beige": "#FEFCF7",
+        "grey-text": "#83888F",
+        "dark-gray-bg": "#2C343E",
+        "dark-bluish-gray-text": "#333D4B",
+        "light-salmon": "#FDD6BA",
+        "primary-green": "#0E8784",
+        "secondary-green": "#66D2CF",
+      },
+    },
   },
   plugins: [],
 }
