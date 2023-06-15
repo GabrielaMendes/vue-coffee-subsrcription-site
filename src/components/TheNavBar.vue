d9901abffc09ace941c74674cf2b5e3543d0ff24
Gabriela <gdemelo.mendes@gmail.com>
2023-06-15 17:17:49 -0300

    refactor and fix: grouped some styles in css file and fixed overlay position for mobile

--- src/components/TheNavBar.vue
+++ src/components/TheNavBar.vue
@@ -16,19 +16,19 @@ const modalNav = ref(false)
     <nav class="hidden change-menu:flex gap-12">
       <RouterLink to="/">
         <span
-          class="uppercase decoration-none font-bold font-barlow text-sm text-grey-text hover:text-dark-bluish-gray-text"
+          class="menu-item"
           >home</span
         >
       </RouterLink>
       <RouterLink to="/">
         <span
-          class="uppercase decoration-none font-bold font-barlow text-sm text-grey-text hover:text-dark-bluish-gray-text"
+          class="menu-item"
           >about</span
         >
       </RouterLink>
       <RouterLink to="/">
         <span
-          class="uppercase decoration-none font-bold font-barlow text-sm text-grey-text hover:text-dark-bluish-gray-text"
+          class="menu-item"
           >create your plan</span
         >
       </RouterLink>
@@ -39,30 +39,30 @@ const modalNav = ref(false)
       <IconHamburger v-if="!modalNav" class="transform scale-150" />
       <IconClose v-else class="transform scale-150" />
       
-      <div class="absolute w-full top-full left-0">
+      <div v-if="modalNav" class="absolute w-full top-full left-0">
         <nav
-          v-if="modalNav"
-          class="flex flex-col items-center justify-center gap-10 menu-gradient py-10"
+          class="bg-light-beige flex flex-col items-center justify-center gap-10 py-10"
         >
           <RouterLink to="/">
             <span
-              class="capitalize decoration-none font-bold font-fraunces text-2xl text-dark-bluish-gray-text"
+              class="mobile-menu-item"
               >home</span
             >
           </RouterLink>
           <RouterLink to="/">
             <span
-              class="capitalize decoration-none font-bold font-fraunces text-2xl text-dark-bluish-gray-text"
+              class="mobile-menu-item"
               >about us</span
             >
           </RouterLink>
           <RouterLink to="/">
             <span
-              class="capitalize decoration-none font-bold font-fraunces text-2xl text-dark-bluish-gray-text"
+              class="mobile-menu-item"
               >create your plan</span
             >
           </RouterLink>
         </nav>
+        <div class=" h-[calc(100svh_-_395px)] menu-gradient"></div>
       </div>
     </div>
   </header>
