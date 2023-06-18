7b8e751c76ba81901947ed6024a459e1d52528d5
Gabriela <gdemelo.mendes@gmail.com>
2023-06-18 12:16:26 -0300

    layout: created homepage benefits section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,8 @@
 <script setup>
-import { RouterLink } from "vue-router";
+import { RouterLink } from "vue-router"
+import IconCoffeeBean from "@/components/icons/IconCoffeeBean.vue";
+import IconGift from "@/components/icons/IconGift.vue";
+import IconTruck from "@/components/icons/IconTruck.vue";
 
 const coffees = [
   {
@@ -22,7 +25,27 @@ const coffees = [
     image: "src/assets/images/home/desktop/image-danche.png",
     description: "Ethiopian hand-harvested blend densely packed with vibrant fruit notes",
   },
-]
+];
+
+const benefits = [
+  {
+    name: "Best quality",
+    image: IconCoffeeBean,
+    description:
+      "Discover an endless variety of the world’s best artisan coffee from each of our roasters.",
+  },
+  {
+    name: "Exclusive benefits",
+    image: IconGift,
+    description:
+      "Special offers and swag when you subscribe, including 30% off your first shipment.",
+  },
+  {
+    name: "Free shipping ",
+    image: IconTruck,
+    description: "We cover the cost and coffee is delivered fast. Peak freshness: guaranteed.",
+  },
+];
 </script>
 
 <template>
@@ -32,17 +55,13 @@ const coffees = [
       <div
         class="text-light-beige w-full text-center sm:text-left sm:w-[398px] lg:w-[493px] h-full flex flex-col justify-center items-center md:items-left gap-10"
       >
-        <h1 class="text-[40px] sm:text-5xl lg:text-7xl">
-          Great coffee made simple.
-        </h1>
+        <h1 class="text-[40px] sm:text-5xl lg:text-7xl">Great coffee made simple.</h1>
         <p>
           Start your mornings with the world's best coffees. Try our expertly curated artisan
           coffees from our best roasters delivered directly to your door, at your schedule.
         </p>
         <RouterLink role="button" class="app-button sm:mr-auto" to="/">
-          <span class="text-lg font-fraunces font-black">
-            Create your plan
-          </span>
+          <span class="text-lg font-fraunces font-black"> Create your plan </span>
         </RouterLink>
       </div>
     </section>
@@ -50,22 +69,58 @@ const coffees = [
     <!-- Coffee Collection -->
     <section class="w-full relative">
       <div class="w-full sm:absolute sm:top-6 lg:top-4">
-        <h1 class="extra-padding w-full mb-10 text-grey-text sm:max-lg:p-0 text-center text-3xl xs:text-[min(10vw,_150px)] relative sm:opacity-50 after:collection-overlay">
+        <h2
+          class="extra-padding w-full mb-10 text-grey-text sm:max-lg:p-0 text-center text-3xl xs:text-[min(10vw,_150px)] relative sm:opacity-50 after:collection-overlay"
+        >
           our collection
-        </h1>
+        </h2>
       </div>
-      
-      <div class="lg:flex gap-[30px] justify-between items-start extra-padding relative z-10 sm:pt-14">
-        <div v-for="coffee in coffees" :key="coffee.name" class="max-lg:mb-14 max-lg:last:mb-0 flex flex-1 flex-col items-center justify-center gap-6 sm:gap-10 lg:gap-[71px] sm:max-lg:flex-row">
-          
-          <img :src="coffee.image" alt="coffee illustration" class="w-[200px] sm:w-[255px]">
-          
+
+      <div
+        class="lg:flex gap-[30px] justify-between items-start extra-padding relative z-10 sm:pt-14"
+      >
+        <div
+          v-for="coffee in coffees"
+          :key="coffee.name"
+          class="max-lg:mb-14 max-lg:last:mb-0 flex flex-1 flex-col items-center justify-center gap-6 sm:gap-10 lg:gap-[71px] sm:max-lg:flex-row"
+        >
+          <img :src="coffee.image" alt="coffee illustration" class="w-[200px] sm:w-[255px]" />
+
           <div class="text-center sm:max-lg:text-left">
-            <h2 class="text-2xl mb-4 sm:mb-6">{{ coffee.name }}</h2>
+            <h3 class="text-2xl mb-4 sm:mb-6">{{ coffee.name }}</h3>
             <p>{{ coffee.description }}</p>
           </div>
         </div>
       </div>
+    </section>
+
+    <!-- Why Choose Us -->
+    <section class="w-full text-light-beige text-center">
+      <div class="bg-dark-grey-bg  extra-padding rounded-lg  w-full h-[902px] sm:h-[573px] overflow-visible">
+        <div class="py-16 lg:py-20 lg:w-1/2 mx-auto">
+          <h2 class="mb-6 text-[28px] sm:text-[32px] lg:text-[40px]">Why choose us?</h2>
+          <p>
+            A large part of our role is choosing which particular coffees will be featured in our
+            range. This means working closely with the best coffee growers to give you a more
+            impactful experience on every level.
+          </p>
+        </div>
+      </div>
+
+        <div class="extra-padding grid grid-rows-3 lg:grid-rows-1 lg:grid-cols-3 gap-6 -mt-[620px] sm:-mt-[320px] lg:-mt-[250px]">
+          <div
+            v-for="benefit in benefits"
+            :key="benefit.name"
+            class="bg-primary-green rounded-md text-center text-light-beige p-12 last:pt-[70px] last:sm:max-lg:pt-12 flex flex-col sm:max-lg:flex-row items-center gap-14 sm:max-lg:gap-12"
+          >
+            <component :is="benefit.image" class="shrink-0"></component>
+            
+            <div class="sm:max-lg:text-left">
+              <h3 class="mb-6 text-2xl">{{ benefit.name }}</h3>
+              <p>{{ benefit.description }}</p>
+            </div>
+          </div>
+        </div>
     </section>
   </main>
 </template>
