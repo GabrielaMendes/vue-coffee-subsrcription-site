eafcc245b0ccda956b9437353f0b2825c817cd45
Gabriela <gdemelo.mendes@gmail.com>
2023-06-19 21:28:48 -0300

    fix: fixed wrong class name on hero section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -74,7 +74,7 @@ const steps = [
     <!-- Hero -->
     <section class="w-full hero-bg rounded-lg extra-padding">
       <div
-        class="text-light-beige w-full text-center sm:text-left sm:w-[398px] lg:w-[493px] h-full flex flex-col justify-center items-center md:items-left gap-10"
+        class="text-light-beige w-full text-center sm:text-left sm:w-[398px] lg:w-[493px] h-full flex flex-col justify-center items-center sm:items-start gap-10"
       >
         <h1 class="text-[40px] sm:text-5xl lg:text-7xl">Great coffee made simple.</h1>
         <p>
