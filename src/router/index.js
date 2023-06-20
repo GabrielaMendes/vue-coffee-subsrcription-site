323e71ea17343e39acc8a925fc4e9fef51cd1f4c
Gabriela <gdemelo.mendes@gmail.com>
2023-06-20 11:35:42 -0300

    feature: implemented scroll behavior to go to top of the page when navigating

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,37 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: HomeView,
+  },
+  {
+    path: "/about-us",
+    name: "about",
+    component: () => import("../views/AboutView.vue"),
+  },
+  {
+    path: "/create-plan",
+    name: "create",
+    component: () => import("../views/CreatePlanView.vue"),
+  },
+];
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: HomeView,
-    },
-    {
-      path: "/about-us",
-      name: "about",
-      component: () => import("../views/AboutView.vue"),
-    },
-    {
-      path: "/create-plan",
-      name: "create",
-      component: () => import("../views/CreatePlanView.vue"),
-    },
-  ],
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    }
+    if (to.hash) {
+      return;
+    }
+
+    return { top: 0, left: 0 };
+  },
 });
 
 export default router;
