86bc7451af2fc2795c2dbca90b198de1e54b28cc
Gabriela <gdemelo.mendes@gmail.com>
2023-06-21 09:11:56 -0300

    feature: added transition when navigating between routes

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <script setup>
-import { RouterView } from 'vue-router'
-import TheNavBar from './components/TheNavBar.vue';
-import TheFooter from './components/TheFooter.vue';
+import { RouterView } from "vue-router";
+import TheNavBar from "./components/TheNavBar.vue";
+import TheFooter from "./components/TheFooter.vue";
 </script>
 
 <template>
   <div class="w-full max-w-[1400px] px-10 mx-auto">
     <TheNavBar />
-    <RouterView />
+    <transition 
+      mode="out-in"
+      enter-from-class="opacity-0"
+      leave-to-class="opacity-0"
+      enter-active-class="transition-opacity duration-500 ease-in"
+      leave-active-class="transition-opacity duration-500 ease-in"
+    >
+      <RouterView />
+    </transition>
     <TheFooter />
   </div>
 </template>
