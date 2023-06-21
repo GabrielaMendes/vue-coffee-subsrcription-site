b71373942ad0dd2b446ab870cd46541ca3470973
Gabriela <gdemelo.mendes@gmail.com>
2023-06-21 09:37:17 -0300

    layout: created plan page hero and added style to main css

--- src/views/CreatePlanView.vue
+++ src/views/CreatePlanView.vue
@@ -1,5 +1,15 @@
 <template>
   <main>
-    <div>CREATE PLAN PAGE</div>
+    <!-- Hero -->
+    <section class="w-full hero-plan-bg rounded-lg extra-padding">
+      <div
+        class="text-light-beige w-full text-center sm:text-left sm:w-[398px] lg:w-[493px] h-full flex flex-col justify-center items-center sm:items-start gap-10"
+      >
+        <h2 class="text-[40px] sm:text-5xl lg:text-7xl">Create a plan</h2>
+        <p>
+          Build a subscription plan that best fits your needs. We offer an assortment of the best artisan coffees from around the globe delivered fresh to your door.
+        </p>
+      </div>
+    </section>
   </main>
 </template>
