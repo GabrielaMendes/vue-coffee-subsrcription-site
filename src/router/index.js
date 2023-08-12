f0bc37126d5198325d8a70e396f75fd830c93d01
Gabriela <gdemelo.mendes@gmail.com>
2023-08-12 16:07:39 -0300

    feature: changed scroll behavior to go top on refresh

--- src/router/index.js
+++ src/router/index.js
@@ -22,19 +22,13 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
-  scrollBehavior(to, from, savedPosition) {
-    if (savedPosition) {
-      return savedPosition;
-    }
+  scrollBehavior(to, from) {
     if (from.path === to.path) {
       return {
         top: 0,
         left: 0,
         behavior: "smooth",
       };
-    }
-    if (to.hash) {
-      return;
     }
 
     return { top: 0, left: 0 };
